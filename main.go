ff31ab999481d4e416d6266057be1012358f50a1
Long Tran <long.thanh.tran@gmail.com>
2023-06-17 14:47:28 +0700

    resolve #2

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"crypto/sha256"
+	"crypto/subtle"
 	"fmt"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/keyauth"
 	"github.com/longthanhtran/go-smart-ac/api"
 	"github.com/longthanhtran/go-smart-ac/database"
+	"os"
 
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
@@ -26,9 +30,26 @@ func initDb() {
 	fmt.Println("Database migrated")
 }
 
+var (
+	apiKey = os.Getenv("X-API-KEY")
+)
+
+func validateAPIKey(_ *fiber.Ctx, key string) (bool, error) {
+	hashedAPIKey := sha256.Sum256([]byte(apiKey))
+	hashedKey := sha256.Sum256([]byte(key))
+
+	if subtle.ConstantTimeCompare(hashedAPIKey[:], hashedKey[:]) == 1 {
+		return true, nil
+	}
+	return false, keyauth.ErrMissingOrMalformedAPIKey
+}
 func main() {
 	app := fiber.New()
 	app.Use(cors.New())
+	app.Use(keyauth.New(keyauth.Config{
+		Validator: validateAPIKey,
+	}))
+
 	initDb()
 
 	app.Post("/api/acs", api.Create)
